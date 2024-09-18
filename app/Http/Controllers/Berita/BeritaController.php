e76c6d83086fe64c1106b9500452fc69774c7118
syukron <syukron488@gmail.com>
2024-09-18 11:54:43 +0700

    implement page berita

--- app/Http/Controllers/Berita/BeritaController.php
+++ app/Http/Controllers/Berita/BeritaController.php
@@ -10,10 +10,16 @@ class BeritaController extends Controller
 {
     public function index()
     {
-        // $candidates = Candidate::with('users')->paginate(5);
-        // $jumlah = User::where('status','SUDAH')->count();
-        // $candidates = Candidate::get();
-        return view('berita.index');
+        $artikel = Artikel::leftjoin('kategori_artikel','kategori_artikel.id','=','artikel.kategori_id')
+                    ->select(
+                        'kategori_artikel.nama as nama_kategori',
+                        'artikel.judul',
+                        'artikel.slug',
+                        'artikel.deskripsi',
+                        'artikel.gambar'
+                    )
+                    ->get();
+        return view('berita.index', compact('artikel'));
     }
 
     public function show(Request $request, $slug)
--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -5,11 +5,11 @@
 <!-- Page Title -->
 <div class="page-title" data-aos="fade">
     <div class="container d-lg-flex justify-content-between align-items-center">
-        <h1 class="mb-2 mb-lg-0">Berita</h1>
+        <h1 class="mb-2 mb-lg-0">Semua Artikel</h1>
         <nav class="breadcrumbs">
             <ol>
-                <li><a href="index.html">Beranda</a></li>
-                <li class="current">Berita</li>
+                <li><a href="/berita">Beranda</a></li>
+                <li class="current">Artikel</li>
             </ol>
         </nav>
     </div>
@@ -22,86 +22,48 @@
         <div class="row mb-5 mt-3">
             <div class="card my-4">
                 <div class="card">
-                    <img src="https://img.freepik.com/free-photo/hacker-hood-stealing-data-from-server-room-cybercrime-concept_1142-58515.jpg?t=st=1724609119~exp=1724612719~hmac=0ec2ae27442bb53dc14c22a1342cd69db6215d91c54d3f2564f7caf3aed33c07&w=740" alt="Peringatan Keamanan" class="img-fluid" style="max-height: 400px; object-fit: cover">
+                    <img src="{{ '/storage/'.$artikel[0]->gambar }}" alt="Peringatan Keamanan" class="img-fluid" style="max-height: 400px; object-fit: cover">
                 </div>
-
                 <div class="card-body text-center">
                     <h3 class="card-title">
-                        <a href="#!" class="text-decoration-none text-dark">Imbauan Keamanan Modus Penipuan Menggunakan Undangan Pernikahan Elektronik APK</a>
+                        <a href="#!" class="text-decoration-none text-dark">{{ $artikel[0]->judul }}</a>
                     </h3>
                     <p>
                         <small class="text-muted">
-                            By <a href="javascript:void(0)" class="text-decoration-none">Mas Admin</a> in <a href="#!" class="text-decoration-none">Peringatan Keamanan</a> at <span class="text-dark">07 May 2023</span>
+                            <a href="#!" class="text-decoration-none">{{ $artikel[0]->nama_kategori }}</a> - <span class="text-dark">{{ \Carbon\Carbon::parse($artikel[0]->created_at)->format('j F Y') }}</span>
                         </small>
                     </p>
-                    <p class="card-text">Waspadai modus penipuan menggunakan berkas Android Package Kit (.apk) undangan pernikahan elektronik. Berkas .apk tersebut dikirimkan melalui aplikasi perpesanan WhatsApp. Jika penerima pesan mengakses berkas .apk tersebut maka penjahat akan…</p>
+                    <p class="card-text">
+                        {!! Str::limit( $artikel[0]->deskripsi, 150) !!}
+                    </p>
 
-                    <a href="#!" class="text-decoration-none btn btn-primary">Read more</a>
+                    <a href="{{ route('berita-detail', $artikel[0]->slug) }}" class="text-decoration-none btn btn-primary">Selengkapnya</a>
                 </div>
             </div>
+            @foreach ($artikel as $item)
+            @if ($loop->first) @continue @endif
             <div class="col-lg-4 col-md-6 mb-5">
                 <div class="card card-effect" style="box-shadow: var(--box-shadow);background-color: #fff;padding: 25px;transition: all 0.35s ease;">
                     <div class="position-absolute bg-dark rounded px-3 py-2" style="background-color: rgba(0,0,0,0.7)">
-                        <a href="#!" class="text-white text-decoration-none">Peringatan Keamanan</a>
+                        <a href="#!" class="text-white text-decoration-none">{{ $item->nama_kategori }}</a>
                     </div>
-                    <img src="https://img.freepik.com/free-vector/flat-design-ransomware-infographic_23-2149381252.jpg?t=st=1724605052~exp=1724608652~hmac=da8e70f9e0f9f0720cfa1493a0f61aa30547f3072d024510fe76ac5682d4248f&w=740" alt="Peringatan Keamanan">
+                    <img src="{{ '/storage/'.$item->gambar }}" alt="Peringatan Keamanan">
 
                     <div class="card-body">
                         <h5 class="card-title">
-                            <a href="#!">Ransomware LockBit Kembangkan Versi Linuxnya</a>
+                            <a href="#!">{{ $item->judul }}</a>
                         </h5>
                         <p>
                             <small class="text-muted">
-                                By <a href="javascript:void(0)" class="text-decoration-none"></a> at <span class="text-dark">17 February 2022</span>
+                                at <span class="text-dark">{{ \Carbon\Carbon::parse($item->created_at)->format('j F Y') }}</span>
                             </small>
                         </p>
-                        <p class="card-text">LockBit adalah salah satu keluarga ransomware paling produktif. Ransomware ini menjadi lebih berbahaya dengan varian yang berfokus pada Linux dan VMware ESXi. Versi ini diberi nama LockBit Linux-ESXi...</p>
-                        <a href="#!" class="btn btn-primary">Read More</a>
+                        <p class="card-text">{!! Str::limit( $item->deskripsi, 150) !!}</p>
+                        <a href="#!" class="btn btn-primary">Selengkapnya</a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 mb-5">
-                <div class="card card-effect" style="box-shadow: var(--box-shadow);background-color: #fff;padding: 25px;transition: all 0.35s ease;">
-                    <div class="position-absolute bg-dark rounded px-3 py-2" style="background-color: rgba(0,0,0,0.7)">
-                        <a href="#!" class="text-white text-decoration-none">Peringatan Keamanan</a>
-                    </div>
-                    <img src="https://img.freepik.com/free-vector/flat-design-ransomware-infographic_23-2149381252.jpg?t=st=1724605052~exp=1724608652~hmac=da8e70f9e0f9f0720cfa1493a0f61aa30547f3072d024510fe76ac5682d4248f&w=740" alt="Peringatan Keamanan">
-
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <a href="#!">Ransomware LockBit Kembangkan Versi Linuxnya</a>
-                        </h5>
-                        <p>
-                            <small class="text-muted">
-                                By <a href="javascript:void(0)" class="text-decoration-none"></a> at <span class="text-dark">17 February 2022</span>
-                            </small>
-                        </p>
-                        <p class="card-text">LockBit adalah salah satu keluarga ransomware paling produktif. Ransomware ini menjadi lebih berbahaya dengan varian yang berfokus pada Linux dan VMware ESXi. Versi ini diberi nama LockBit Linux-ESXi...</p>
-                        <a href="#!" class="btn btn-primary">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5">
-                <div class="card card-effect" style="box-shadow: var(--box-shadow);background-color: #fff;padding: 25px;transition: all 0.35s ease;">
-                    <div class="position-absolute bg-dark rounded px-3 py-2" style="background-color: rgba(0,0,0,0.7)">
-                        <a href="#!" class="text-white text-decoration-none">Peringatan Keamanan</a>
-                    </div>
-                    <img src="https://img.freepik.com/free-vector/flat-design-ransomware-infographic_23-2149381252.jpg?t=st=1724605052~exp=1724608652~hmac=da8e70f9e0f9f0720cfa1493a0f61aa30547f3072d024510fe76ac5682d4248f&w=740" alt="Peringatan Keamanan">
-
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <a href="#!">Ransomware LockBit Kembangkan Versi Linuxnya</a>
-                        </h5>
-                        <p>
-                            <small class="text-muted">
-                                By <a href="javascript:void(0)" class="text-decoration-none"></a> at <span class="text-dark">17 February 2022</span>
-                            </small>
-                        </p>
-                        <p class="card-text">LockBit adalah salah satu keluarga ransomware paling produktif. Ransomware ini menjadi lebih berbahaya dengan varian yang berfokus pada Linux dan VMware ESXi. Versi ini diberi nama LockBit Linux-ESXi...</p>
-                        <a href="#!" class="btn btn-primary">Read More</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 
